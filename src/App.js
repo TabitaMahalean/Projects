4070d4f0aa2f3f21a434f250f7ec38ef9e1f394e
TabitaMahalean <100995196+TabitaMahalean@users.noreply.github.com>
2022-06-20 15:43:44 +0200

    initial project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="container">
+        <div className="top">
+          <div className="location">
+            <input
+              className="input"
+              type="text"
+              placeholder="&#128269; Enter Location"
+            ></input>
+            <button className="searchButton" type="button">
+              Search
+            </button>
+            <p>Texas</p>
+          </div>
+          <div className="temp">
+            <h2>29°C</h2>
+            <p>4-DAY Forecast</p>
+          </div>
+          <div className="description">
+            <p>Cloudy</p>
+          </div>
+        </div>
+        <div className="details">
+          <div className="feels">
+            <p className="bold">30°C</p>
+            <p>Feels like</p>
+          </div>
+          <div className="humidity">
+            <p className="bold">18%</p>
+            <p>Humidity</p>
+          </div>
+          <div className="wind">
+            <p className="bold">12 MPH</p>
+            <p>Wind Speed</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
